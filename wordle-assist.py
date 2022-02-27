bd29aa0a67945235d3ae23305914ac926cf4c9c6
Ross Messing <rmessing@squarespace.com>
2022-02-27 13:45:43 -0500

    Small refactor - modularization.  Grabbing all wordlists to be less cheating-y

--- wordle-assist.py
+++ wordle-assist.py
@@ -5,22 +5,93 @@ import random
 
 import wget
 
+# this wordlist is the list of all possible gueses that aren't solutions, as derived from the original game's javascript source
+ALLOWED_GUESSES_URL = "https://gist.github.com/cfreshman/dec102adb5e60a8299857cbf78f6cf57/raw/15ec4eb961a969d6e263cea4d5b4a180bdeee7bd/allowed.txt"
 # this wordlist is the list of all possible solutions, as derived from the original game's javascript source
-WORD_LIST_URL = "https://gist.githubusercontent.com/cfreshman/a03ef2cba789d8cf00c08f767e0fad7b/raw/5d752e5f0702da315298a6bb5a771586d6ff445c/wordle-answers-alphabetical.txt"
+ALLOWED_ANSWERS_URL = "https://gist.github.com/cfreshman/dec102adb5e60a8299857cbf78f6cf57/raw/15ec4eb961a969d6e263cea4d5b4a180bdeee7bd/answers.txt"
+ALLOWED_GUESSES_FILENAME = "guesses.txt"
+ALLOWED_ANSWERS_FILENAME = "answers.txt"
+# We'll combine them into
 WORD_LIST_FILENAME="words.txt"
 WORD_LENGTH = 5
 RANDOM_SEED = 42
 
 random.seed(RANDOM_SEED)
 
-def load_dictionary(url=WORD_LIST_URL, filename=WORD_LIST_FILENAME, word_length=WORD_LENGTH):
+
+def read_word_list_file(filename, word_length=WORD_LENGTH):
     """
-    Returns a list of words of length word_length from the words in filename.  If filename isn't present, downloads it.
+    Reads a text word list into a list of words, each of which have length word_length
     """
-    if not os.path.exists(WORD_LIST_FILENAME):
-        wget.download(WORD_LIST_URL, WORD_LIST_FILENAME)
-    with open(WORD_LIST_FILENAME, 'r') as fh:
-        return [line.strip() for line in fh if len(line.strip()) == WORD_LENGTH]
+    with open(filename, 'r') as fh:
+        return [line.strip() for line in fh if len(line.strip()) == word_length]
+
+def load_word_list(word_length=WORD_LENGTH):
+    """
+    Returns a list of words of length word_length from the words in the word lists.  
+    If word lists aren't present, downloads them.
+    Sorts the returned word list, just to ensure we don't use the knowledge of which words can be guessed but cannot be solutions
+    """
+    if not os.path.exists(ALLOWED_GUESSES_FILENAME):
+        wget.download(ALLOWED_GUESSES_URL, ALLOWED_GUESSES_FILENAME)
+    if not os.path.exists(ALLOWED_ANSWERS_FILENAME):
+        wget.download(ALLOWED_ANSWERS_URL, ALLOWED_ANSWERS_FILENAME)
+    return sorted(read_word_list_file(ALLOWED_GUESSES_FILENAME) + read_word_list_file(ALLOWED_ANSWERS_FILENAME))
+
+
+def process_guess(word_list, guess, result):
+    """
+    Takes in the current list of possible words, a guess string, and a result string
+    Returns the new list of possible words
+    """
+    assert len(guess) == WORD_LENGTH, f"Word ({guess}) must have length {WORD_LENGTH}"
+    assert len(guess) == len(result), f'Result ({result}) must have the same length as word ({word})'
+    for r in result:
+        assert r in {'0', '1', '2'}, "Result characters must be 0, 1 or 2"
+    to_remove = set()
+    wrong_pos = set()
+    right_pos = set()
+    for position, (letter, result) in enumerate(zip(guess, result)):
+        if result == '0':  # letter not in word at all
+            to_remove.add(letter)
+        elif result == '1':  # wrong position
+            wrong_pos.add((letter, position))
+        else:  # right position
+            right_pos.add((letter, position))
+    
+    new_word_list = []
+    for word in word_list:
+        retain = True
+        # first check if it's got a letter to be removed
+        for c in to_remove:
+            if c in word:
+                retain = False
+                break
+        if not retain:
+            continue
+        # now check if it's got a letter in the right place
+        for l, i in right_pos:
+            if word[i] != l:
+                retain = False
+                break
+        if not retain:
+            continue
+        # now check if it DOESN'T have a letter in the wrong place, but DOES have that letter
+        for l, i in wrong_pos:
+            if (word[i] == l) or (l not in word):
+                retain = False
+                break
+        if not retain:
+            continue
+        new_word_list.append(word)
+    return new_word_list
+
+
+def suggest_guess(word_list):
+    """
+    Suggests a guess from the set of possible words
+    """
+    return random.choice(word_list)
 
 
 class WordleShell(cmd.Cmd):
@@ -37,7 +108,7 @@ class WordleShell(cmd.Cmd):
 
     def do_suggest(self, arg):
         'Suggest a word to guess.'
-        print(random.choice(self.word_list))
+        print(suggest_guess(self.word_list))
 
     def do_guess(self, arg):
         '''
@@ -50,47 +121,8 @@ class WordleShell(cmd.Cmd):
         arg_split = arg.split()
         word = arg_split[0].strip()
         result = arg_split[1].strip()
-        assert len(word) == WORD_LENGTH, f"Word ({word}) must have length {WORD_LENGTH}"
-        assert len(word) == len(result), f'Result ({result}) must have the same length as word ({word})'
-        for r in result:
-            assert r in {'0', '1', '2'}, "Result characters must be 0, 1 or 2"
-        to_remove = set()
-        wrong_pos = set()
-        right_pos = set()
-        for position, (letter, result) in enumerate(zip(word, result)):
-            if result == '0':  # letter not in word at all
-                to_remove.add(letter)
-            elif result == '1':  # wrong position
-                wrong_pos.add((letter, position))
-            else:  # right position
-                right_pos.add((letter, position))
         old_word_list_size = len(self.word_list)
-        new_word_list = []
-        for word in self.word_list:
-            retain = True
-            # first check if it's got a letter to be removed
-            for c in to_remove:
-                if c in word:
-                    retain = False
-                    break
-            if not retain:
-                continue
-            # now check if it's got a letter in the right place
-            for l, i in right_pos:
-                if word[i] != l:
-                    retain = False
-                    break
-            if not retain:
-                continue
-            # now check if it DOESN'T have a letter in the wrong place, but DOES have that letter
-            for l, i in wrong_pos:
-                if (word[i] == l) or (l not in word):
-                    retain = False
-                    break
-            if not retain:
-                continue
-            new_word_list.append(word)
-        self.word_list = new_word_list
+        self.word_list = process_guess(self.word_list, word, result)
         print(f'This guess reduced the set of possible solutions from {old_word_list_size} candidates to {len(self.word_list)}')
     
     def do_exit(self, arg):
@@ -99,4 +131,4 @@ class WordleShell(cmd.Cmd):
 
 
 if __name__ == '__main__':
-    WordleShell(word_list=load_dictionary()).cmdloop()
+    WordleShell(word_list=load_word_list()).cmdloop()
